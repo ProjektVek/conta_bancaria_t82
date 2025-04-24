965866af2c7e70c0059ceba5fc717c7b33dd7c6d
Victor Hugo <projektvek@gmail.com>
2025-04-24 14:39:12 -0300

    :sparkles: Adiciona cores ao projeto

---
+++ src/conta_bancaria/Menu.java
@@ -0,0 +1,12 @@
+package conta_bancaria;
+
+import conta_bancaria.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println(Cores.TEXT_GREEN_BOLD + Cores.ANSI_BLACK_BACKGROUND + "Olá Mundo!                ");
+		System.out.println(Cores.TEXT_RESET + "Olá");
+	}
+
+}
