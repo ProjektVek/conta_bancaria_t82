057f8829776f6b10b6619a38abfb89e73740b0b1
Victor Hugo <projektvek@gmail.com>
2025-04-25 15:29:32 -0300

    :sparkles: Classe Conta Criada

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,12 +1,23 @@
 package conta_bancaria;
 
+import conta_bancaria.model.Conta;
 import conta_bancaria.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		System.out.println(Cores.TEXT_GREEN_BOLD + Cores.ANSI_BLACK_BACKGROUND + "Olá Mundo!                ");
-		System.out.println(Cores.TEXT_RESET + "Olá");
+		System.out.println(Cores.TEXT_GREEN_BOLD + Cores.ANSI_BLACK_BACKGROUND);
+		
+		Conta conta = new Conta(1, 1, 1, "Victor", 100000000);
+		conta.visualizar();
+		
+		conta.depositar(80000);
+		conta.visualizar();
+		
+		conta.sacar(1000000000);
+		conta.sacar(50000);
+		conta.visualizar();
+		
 	}
 
 }
